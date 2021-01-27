a73ab1446a4e681f3ed1178fa5960718be4ee259
eken <luwj@chexiu.cn>
2021-01-27 10:29:22 +0800

    路由身份校验

--- api/mini/route/route.go
+++ api/mini/route/route.go
@@ -13,14 +13,14 @@ func Init(eg *gin.Engine) *gin.Engine {
 		return
 	})
 
-	eg.POST("goods/update-number", c.GoodsUpdateNumber)
-	eg.POST("goods/add", c.GoodsAdd)
 	eg.GET("wx-fans/get-session", c.GetSession)
 
 	eg = middleware.Init(eg)
 
 	eg.GET("goods/number", c.GoodsNumber)
 	eg.GET("goods/keyword-list", c.GoodsKeywordList)
+	eg.POST("goods/update-number", c.GoodsUpdateNumber)
+	eg.POST("goods/add", c.GoodsAdd)
 
 	eg.POST("wx-fans/set-user-info", c.SetUserInfo)
 
